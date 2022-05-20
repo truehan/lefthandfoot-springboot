6abb9e6a46e64eba84b3ef830f0c9c2c9ae78373
jeff <jeff.rong@truehan.com>
2022-05-20 14:48:11 +0800

    Test

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -8,10 +8,7 @@ import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
 import com.tencent.wxcloudrun.service.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -24,59 +21,72 @@ import java.util.List;
 @Slf4j
 public class CounterController {
 
-  final CounterService counterService;
+    final CounterService counterService;
 
-  public CounterController(@Autowired CounterService counterService) {
-    this.counterService = counterService;
-  }
-
-
-  /**
-   * 获取当前计数
-   * @return API response json
-   */
-  @GetMapping(value = "/api/count")
-  ApiResponse get() {
-    log.info("/api/count get request");
-    Optional<Counter> counter = counterService.getCounter(1);
-    Integer count = 0;
-    if (counter.isPresent()) {
-      count = counter.get().getCount();
+    public CounterController(@Autowired CounterService counterService) {
+        this.counterService = counterService;
     }
 
-    return ApiResponse.ok(count);
-  }
 
+    /**
+     * 获取当前计数
+     *
+     * @return API response json
+     */
+    @GetMapping(value = "/api/count")
+    ApiResponse get() {
+        log.info("/api/count get request");
+        Optional<Counter> counter = counterService.getCounter(1);
+        Integer count = 0;
+        if (counter.isPresent()) {
+            count = counter.get().getCount();
+        }
 
-  /**
-   * 更新计数，自增或者清零
-   * @param request {@link CounterRequest}
-   * @return API response json
-   */
-  @PostMapping(value = "/api/count")
-  ApiResponse create(@RequestBody CounterRequest request) {
-    log.info("/api/count post request, action: {}", request.getAction());
-
-    Optional<Counter> curCounter = counterService.getCounter(1);
-    if (request.getAction().equals("inc")) {
-      Integer count = 99999999;
-      if (curCounter.isPresent()) {
-        count += curCounter.get().getCount();
-      }
-      Counter counter = new Counter();
-      counter.setId(1);
-      counter.setCount(count);
-      counterService.upsertCount(counter);
-      return ApiResponse.ok(count);
-    } else if (request.getAction().equals("clear")) {
-      if (!curCounter.isPresent()) {
-        return ApiResponse.ok(0);
-      }
-      counterService.clearCount(1);
-      return ApiResponse.ok(0);
-    } else {
-      return ApiResponse.error("参数action错误");
+        return ApiResponse.ok(count);
     }
-  }
-  
+
+
+    /**
+     * 更新计数，自增或者清零
+     *
+     * @param request {@link CounterRequest}
+     * @return API response json
+     */
+    @PostMapping(value = "/api/count")
+    ApiResponse create(@RequestBody CounterRequest request) {
+        log.info("/api/count post request, action: {}", request.getAction());
+
+        Optional<Counter> curCounter = counterService.getCounter(1);
+        if (request.getAction().equals("inc")) {
+            Integer count = 99999999;
+            if (curCounter.isPresent()) {
+                count += curCounter.get().getCount();
+            }
+            Counter counter = new Counter();
+            counter.setId(1);
+            counter.setCount(count);
+            counterService.upsertCount(counter);
+            return ApiResponse.ok(count);
+        } else if (request.getAction().equals("clear")) {
+            if (!curCounter.isPresent()) {
+                return ApiResponse.ok(0);
+            }
+            counterService.clearCount(1);
+            return ApiResponse.ok(0);
+        } else {
+            return ApiResponse.error("参数action错误");
+        }
+    }
+
+
+    /**
+     * 更新计数，自增或者清零
+     *
+     * @return API response json
+     */
+    @RequestMapping(value = "token")
+    boolean token() {
+        return true;
+    }
+
 }
