b4ecd99999c9ac1dbc5dc76c6bced095a71ee983
jeff <jeff.rong@truehan.com>
2022-05-20 16:13:37 +0800

    wxServerValdation

--- src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/CounterController.java
@@ -1,18 +1,18 @@
 package com.tencent.wxcloudrun.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.CounterRequest;
 import com.tencent.wxcloudrun.model.Counter;
 import com.tencent.wxcloudrun.service.CounterService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Optional;
-import java.util.List;
 
 /**
  * counter控制器
@@ -79,15 +79,23 @@ public class CounterController {
     }
 
 
-    /**
-     * 更新计数，自增或者清零
-     *
-     * @return API response json
-     */
-    @GetMapping(value = "token")
-    boolean token() {
-        System.out.println(true + "sdfcsdaf sdaf jhbsdafkjsdbfhjsadfkjhbsda fjkbsdbf jhdsb");
-        return true;
-    }
+    @ResponseBody
+    @RequestMapping(value = "/wxServerValdation")
+    public String wxServerValdation(String signature, String timestamp, String nonce, String echostr){
+        if (ObjectUtils.isEmpty(signature)|| ObjectUtils.isEmpty(timestamp) || ObjectUtils.isEmpty(nonce) || ObjectUtils.isEmpty(echostr)){
+            return "";
+        }
+        ArrayList<String> list=new ArrayList<String>();
+        list.add(nonce);
+        list.add(timestamp);
+        list.add("NvJEBK12");//这是第5步中你设置的Token
+        Collections.sort(list);
+        String sha1Singnature = DigestUtils.sha1Hex(list.get(0)+list.get(1)+list.get(2));
+        if (sha1Singnature.equals(signature)){
+            return echostr;
+        }else {
+            return "";
+        }
 
+    }
 }
